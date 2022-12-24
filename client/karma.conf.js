348ea150c24558200f2d5520366378e803478285
Maksym Charuk <maxcharuk@gmail.com>
2022-12-24 00:47:36 -0500

    Fix issue with missing data pulling during deployment

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/deployment-test'),
+      dir: require('path').join(__dirname, './coverage/hvoya-crm'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
