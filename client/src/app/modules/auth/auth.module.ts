3d792b67b99c14ed93bac87f85910a0f4e82d6bc
Vladyslav Charuk <vlad.charu@gmail.com>
2022-12-28 22:56:50 +0200

    relative path fix

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from 'src/app/auth/sign-in/sign-in.component';
+import { SignInComponent } from '../../auth/sign-in/sign-in.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
