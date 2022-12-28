d0cf8706bddb45f55a898e3df08ad1880d67f587
Vladyslav Charuk <vlad.charu@gmail.com>
2022-12-28 21:07:51 +0200

    sign in page added, primeNG added, auth module added, angular cli/core changed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,8 +8,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ client/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from 'src/app/auth/sign-in/sign-in.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+];
+
+@NgModule({
+  declarations: [SignInComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule, InputTextModule,
+    ReactiveFormsModule,
+    PasswordModule,
+    ButtonModule
+  ],
+  exports: [RouterModule],
+})
+export class AuthModule { }
