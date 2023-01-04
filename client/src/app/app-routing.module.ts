796bff3fbf778aac99c35eb54da5fdf25f9125f8
Vladyslav Charuk <vlad.charu@gmail.com>
2023-01-04 21:57:06 +0200

    signed out guard added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignedInGuard } from './shared/guards/signed-in/signed-in.guard';
+import { SignedOutGuard } from './shared/guards/signed-out/signed-out.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth/sign-in', pathMatch: 'full' },
   {
     path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+    canActivate: [SignedOutGuard]
   },
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
-    canActivate: [SignedInGuard],
+    canActivate: [SignedInGuard]
   }
 ];
 
---
+++ client/src/app/shared/guards/signed-out/signed-out.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { StorageService } from '../../services/storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignedOutGuard implements CanActivate {
+
+  constructor(private storageService: StorageService,
+    private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (!this.storageService.getItem('access_token')) {
+      return true;
+    } else {
+      this.router.navigateByUrl('dashboard');
+      return false;
+    }
+  }
+
+}
