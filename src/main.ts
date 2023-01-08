3c7795174ab111a0ab9df733e1525de21a6a6ecd
Maksym Charuk <maxcharuk@gmail.com>
2023-01-08 14:43:55 -0500

    Test smtp

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
@@ -8,7 +9,7 @@ import * as rateLimit from 'express-rate-limit';
 import * as nocache from 'nocache';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
   app.use(helmet());
--- src/modules/mail/services/template.service.ts
+++ src/modules/mail/services/template.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { join } from 'path';
 import * as Handlebars from 'handlebars';
 
 @Injectable()
 export class TemplateService {
   compile(templatePath: string, context: any): string {
-    const source = readFileSync(
-      resolve(__dirname, '../../../../', templatePath),
-      {
-        encoding: 'utf-8',
-      },
-    );
+    const source = readFileSync(join(__dirname, '../../../../', templatePath), {
+      encoding: 'utf-8',
+    });
     return Handlebars.compile(source)(context);
   }
 }
