08d0f031d9171be827fc91ccba46d6f43593c23f
Maksym Charuk <maxcharuk@gmail.com>
2023-01-09 21:53:50 -0500

    Add Admin page and module

---
+++ client/src/app/modules/admin/admin.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import { AdminRoutingModule } from './admin-routing.module';
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [CommonModule, AdminRoutingModule],
+})
+export class AdminModule {}
