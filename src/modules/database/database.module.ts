ff73fdb49902dc688e71ed957612e3cea1820db9
Vladyslav Charuk <vlad.charu@gmail.com>
2023-01-20 21:09:10 +0200

    subscriptions folder renamed, migrations format added

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { UserSubscriber } from './subscriptions/user.subscriber';
+import { UserSubscriber } from './subscribers/user.subscriber';
 
 @Module({
   imports: [
--- src/modules/database/migrations/1674148831221-AddProductVauriantAndEditProductBaseWithRelation.ts
+++ src/modules/database/migrations/1674148831221-AddProductVauriantAndEditProductBaseWithRelation.ts
@@ -4,8 +4,23 @@ export class AddProductVauriantAndEditProductBaseWithRelation1674148831221 imple
     name = 'AddProductVauriantAndEditProductBaseWithRelation1674148831221'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "product_variant" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "sku" character varying NOT NULL, "name" character varying NOT NULL, "description" character varying, "size" character varying NOT NULL, "color" character varying NOT NULL, "price" numeric(10,2) NOT NULL, "availableItemCount" integer NOT NULL, "baseProductId" integer, CONSTRAINT "UQ_f4dc2c0888b66d547c175f090e2" UNIQUE ("sku"), CONSTRAINT "UQ_6b011a858bfcd83fde87abf1899" UNIQUE ("name"), CONSTRAINT "PK_1ab69c9935c61f7c70791ae0a9f" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "product_variant" ADD CONSTRAINT "FK_9f15354d84c90d612ddb36d68a4" FOREIGN KEY ("baseProductId") REFERENCES "product_base"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `CREATE TABLE "product_variant" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), 
+            "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), 
+            "sku" character varying NOT NULL, 
+            "name" character varying NOT NULL, 
+            "description" character varying, 
+            "size" character varying NOT NULL, 
+            "color" character varying NOT NULL, 
+            "price" numeric(10,2) NOT NULL, 
+            "availableItemCount" integer NOT NULL, 
+            "baseProductId" integer, CONSTRAINT "UQ_f4dc2c0888b66d547c175f090e2" UNIQUE ("sku"), CONSTRAINT "UQ_6b011a858bfcd83fde87abf1899" UNIQUE ("name"), CONSTRAINT "PK_1ab69c9935c61f7c70791ae0a9f" 
+            PRIMARY KEY ("id"))`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "product_variant" ADD CONSTRAINT "FK_9f15354d84c90d612ddb36d68a4" 
+            FOREIGN KEY ("baseProductId") REFERENCES "product_base"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
