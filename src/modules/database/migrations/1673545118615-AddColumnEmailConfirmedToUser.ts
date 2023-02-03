3c646532de8c0f9eb8137dd7e2a914da10733052
Maksym Charuk <maxcharuk@gmail.com>
2023-02-03 16:24:06 -0500

    Format migrations, minor code cleanup

--- src/modules/database/migrations/1673545118615-AddColumnEmailConfirmedToUser.ts
+++ src/modules/database/migrations/1673545118615-AddColumnEmailConfirmedToUser.ts
@@ -1,14 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddColumnEmailConfirmedToUser1673545118615 implements MigrationInterface {
-    name = 'AddColumnEmailConfirmedToUser1673545118615'
+export class AddColumnEmailConfirmedToUser1673545118615
+  implements MigrationInterface
+{
+  name = 'AddColumnEmailConfirmedToUser1673545118615';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "emailConfirmed" boolean NOT NULL DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" 
+        ADD "emailConfirmed" boolean NOT NULL DEFAULT false`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "emailConfirmed"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" 
+        DROP COLUMN "emailConfirmed"`,
+    );
+  }
 }
