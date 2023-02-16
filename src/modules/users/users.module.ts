820a4ba382da2c7f0998bf18713f2d35948867ec
Maksym Charuk <maxcharuk@gmail.com>
2023-02-16 17:40:50 -0500

    Hotfix issue with missing entities for UserModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { OrderDeliveryEntity } from '@entities/order-delivery.entity';
+import { OrderItemEntity } from '@entities/order-item.entity';
+import { OrderEntity } from '@entities/order.entity';
+import { PaymentTransactionEntity } from '@entities/payment-transaction.entity';
 import { UserEntity } from '@entities/user.entity';
 
 import { CaslModule } from '../casl/casl.module';
@@ -8,7 +12,16 @@ import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), CaslModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      OrderEntity,
+      OrderItemEntity,
+      OrderDeliveryEntity,
+      PaymentTransactionEntity,
+    ]),
+    CaslModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
