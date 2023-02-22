214e2c6618291bbb6960d789dd54896e312b6ffd
Vladyslav Charuk <vlad.charu@gmail.com>
2023-02-22 20:44:39 +0200

    e2e added for product edit

--- client/cypress/e2e/products.cy.ts
+++ client/cypress/e2e/products.cy.ts
@@ -1,101 +1,200 @@
-describe( 'Products', () => {
-  let sku: string, name: string, description: string, size: string, color: string, prize: string, baseProductName: string;
+describe('Products', () => {
+  let sku: string, name: string, description: string, size: string, color: string, prize: number, baseProductName: string;
+  let testProductName: string = `Product-${Date.now()}`;
+  let testProductBase: string;
+  let testProductCategory: string;
 
-  beforeEach( () => {
+  beforeEach(() => {
     cy.signInAsAdmin();
-    cy.openProductCreatePage();
     sku = `SKU-${Date.now()}`;
     name = `Product-${Date.now()}`;
     description = 'Product test description';
     size = '1m';
     color = 'red';
-    prize = '100';
+    prize = 1;
     baseProductName = `Base product-${Date.now()}`;
-  } );
+  });
 
-  it( 'Open product create page', () => {
-    cy.get( '[data-cy="product-category-id"]' )
-  } );
+  describe('Create', () => {
+    beforeEach(() => {
+      cy.openProductCreatePage();
+    });
 
-  it( 'Create product with valid data', () => {
-    const categorySelect = 'Віночки і гірлянди';
-    const baseProductSelect = 'Вінок різдвяний литий Premium';
+    it('Open product create page', () => {
+      cy.get('[data-cy="product-category-id"]')
+    });
 
-    cy.fillProductForm( {
-      categorySelect,
-      baseProductSelect,
-      sku,
-      name,
-      description,
-      size,
-      color,
-      prize,
-    } );
+    it('Create product with valid data', () => {
+      const categorySelect = 'Віночки і гірлянди';
+      const baseProductSelect = 'Вінок різдвяний литий Premium';
 
-    cy.get( '[data-cy="product-save-button"]' ).click();
-    cy.get( '[role="alert"]' ).contains(
-      'Продукт успішно створено',
-    );
-  } );
+      cy.fillProductForm({
+        categorySelect,
+        baseProductSelect,
+        sku,
+        name: testProductName,
+        description,
+        size,
+        color,
+        prize,
+      });
 
-  it( 'Create product with value duplicate', () => {
-    const categoryName = 'Віночки і гірлянди';
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно створено',
+      );
+    });
 
-    cy.fillProductForm( {
-      categoryName,
-      baseProductName,
-      sku,
-      name,
-      description,
-      size,
-      color,
-      prize,
-    } );
+    it('Create product with value duplicate', () => {
+      const categoryName = 'Віночки і гірлянди';
 
-    cy.get( '[data-cy="product-save-button"]' ).click();
-    cy.get( '[role="alert"]' ).contains(
-      'duplicate key value violates unique constraint',
-    );
-  } );
+      cy.fillProductForm({
+        categoryName,
+        baseProductName,
+        sku,
+        name,
+        description,
+        size,
+        color,
+        prize,
+      });
 
-  it( 'Create product with new category and base product', () => {
-    const categoryName = `Category-${Date.now()}`;
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'duplicate key value violates unique constraint',
+      );
+    });
 
-    cy.fillProductForm( {
-      categoryName,
-      baseProductName,
-      sku,
-      name,
-      description,
-      size,
-      color,
-      prize,
-    } );
+    it('Create product with new category and base product', () => {
+      const categoryName = `Category-${Date.now()}`;
+      testProductCategory = categoryName;
+      testProductBase = baseProductName;
 
-    cy.get( '[data-cy="product-save-button"]' ).click();
-    cy.get( '[role="alert"]' ).contains(
-      'Продукт успішно створено',
-    );
-  } );
+      cy.fillProductForm({
+        categoryName,
+        baseProductName,
+        sku,
+        name,
+        description,
+        size,
+        color,
+        prize,
+      });
 
-  it( 'Create product with new base product', () => {
-    const categorySelect = 'Віночки і гірлянди';
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно створено',
+      );
+    });
 
-    cy.fillProductForm( {
-      categorySelect,
-      baseProductName,
-      sku,
-      name,
-      description,
-      size,
-      color,
-      prize,
-    } );
+    it('Create product with new base product', () => {
+      const categorySelect = 'Віночки і гірлянди';
 
-    cy.get( '[data-cy="product-save-button"]' ).click();
-    cy.get( '[role="alert"]' ).contains(
-      'Продукт успішно створено',
-    );
-  } );
+      cy.fillProductForm({
+        categorySelect,
+        baseProductName,
+        sku,
+        name,
+        description,
+        size,
+        color,
+        prize,
+      });
 
-} );
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно створено',
+      );
+    });
+  });
+
+  describe('Edit', () => {
+    beforeEach(() => {
+      cy.openProductEditPage();
+    });
+
+    it('Open product edit page', () => {
+      cy.get('[data-cy="product-category-id"]')
+    });
+
+    it('Edit product variant data', () => {
+      cy.fillProductForm({
+        selectedProduct: testProductName,
+        sku,
+        name: testProductName,
+        description,
+        size,
+        color,
+        prize,
+      }, false);
+
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно змінено',
+      );
+
+      cy.reload();
+      cy.selectFromDropdown('[data-cy="product-list"]', testProductName)
+      cy.compareProductVariantFields({
+        sku,
+        name: testProductName,
+        description,
+        size,
+        color,
+        prize,
+      });
+    });
+
+    it('Edit product category and base', () => {
+      cy.fillProductForm({
+        selectedProduct: testProductName,
+        categorySelect: testProductCategory,
+        baseProductSelect: testProductBase,
+        sku,
+        name: testProductName,
+        description,
+        size,
+        color,
+        prize,
+      }, false);
+
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно змінено',
+      );
+
+      cy.reload();
+      cy.selectFromDropdown('[data-cy="product-list"]', testProductName)
+      cy.compareProductCategoryAndBase(testProductCategory, testProductBase);
+    });
+
+    it('Add image to product', () => {
+      cy.selectFromDropdown('[data-cy="product-list"]', testProductName)
+      cy.uploadImageToProduct();
+
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно змінено',
+      );
+
+      cy.reload();
+      cy.selectFromDropdown('[data-cy="product-list"]', testProductName)
+      cy.checkImagesAmount(2);
+    });
+
+    it('Remove existing image from product', () => {
+      cy.selectFromDropdown('[data-cy="product-list"]', testProductName)
+      cy.markFirstImageToBeRemoved();
+
+      cy.get('[data-cy="product-save-button"]').click();
+      cy.get('[role="alert"]').contains(
+        'Продукт успішно змінено',
+      );
+
+      cy.reload();
+      cy.selectFromDropdown('[data-cy="product-list"]', testProductName)
+      cy.checkImagesAmount(1);
+    });
+  });
+
+});
--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -17,16 +17,16 @@ declare global {
         password: string,
         options?: SignInOptions,
       ): typeof signIn;
-      signInAsSuperAdmin( options?: SignInOptions ): typeof signInAsSuperAdmin;
-      signInAsAdmin( options?: SignInOptions ): typeof signInAsAdmin;
-      signInAsUser( options?: SignInOptions ): typeof signInAsUser;
-      signUp( email: string, password: string ): typeof signUp;
-      resetPassword( password: string, token: string ): typeof resetPassword;
+      signInAsSuperAdmin(options?: SignInOptions): typeof signInAsSuperAdmin;
+      signInAsAdmin(options?: SignInOptions): typeof signInAsAdmin;
+      signInAsUser(options?: SignInOptions): typeof signInAsUser;
+      signUp(email: string, password: string): typeof signUp;
+      resetPassword(password: string, token: string): typeof resetPassword;
       registerNewAdmin(
         email: string,
         password: string,
       ): typeof registerNewAdmin;
-      registerNewUser( email: string, password: string ): typeof registerNewUser;
+      registerNewUser(email: string, password: string): typeof registerNewUser;
       logout(): typeof logout;
       openUserMenu(): typeof openUserMenu;
       openProductsMenu(): typeof openProductsMenu;
@@ -34,8 +34,14 @@ declare global {
       openProfilePage(): typeof openProfilePage;
       openSettingsPage(): typeof openSettingsPage;
       openProductCreatePage(): typeof openProductCreatePage;
-      selectFromDropdown( dropdownId: string, value: string ): typeof selectFromDropdown;
-      fillProductForm( product: any ): typeof fillProductForm;
+      openProductEditPage(): typeof openProductEditPage;
+      selectFromDropdown(dropdownId: string, value: string): typeof selectFromDropdown;
+      fillProductForm(product: any, uploadImage?: boolean): typeof fillProductForm;
+      compareProductVariantFields(product: any): typeof compareProductVariantFields;
+      compareProductCategoryAndBase(testProductCategory: string, testProductBase: string): typeof compareProductCategoryAndBase;
+      uploadImageToProduct(): typeof uploadImageToProduct;
+      checkImagesAmount(length: number): typeof checkImagesAmount;
+      markFirstImageToBeRemoved(): typeof markFirstImageToBeRemoved;
     }
   }
 }
@@ -45,110 +51,115 @@ function signIn(
   password: string,
   options: SignInOptions = { full: false },
 ): void {
-  cy.visit( '/' );
-  cy.get( 'input[type=email]' ).type( email );
-  cy.get( 'input[type=password]' ).type( password );
-  cy.get( 'button[type=submit]' ).click();
-  if ( options.full ) {
-    cy.get( '.layout-topbar-logo' ).should( 'be.visible' );
+  cy.visit('/');
+  cy.get('input[type=email]').type(email);
+  cy.get('input[type=password]').type(password);
+  cy.get('button[type=submit]').click();
+  if (options.full) {
+    cy.get('.layout-topbar-logo').should('be.visible');
   }
 }
 
-function signInAsSuperAdmin( options?: SignInOptions ): void {
-  cy.fixture( 'auth.json' )
-    .as( 'authData' )
-    .then( ( { credentials } ) => {
+function signInAsSuperAdmin(options?: SignInOptions): void {
+  cy.fixture('auth.json')
+    .as('authData')
+    .then(({ credentials }) => {
       cy.signIn(
         credentials.superAdmin.email,
         credentials.superAdmin.password,
         options,
       );
-    } );
+    });
 }
 
-function signInAsAdmin( options?: SignInOptions ): void {
-  cy.fixture( 'auth.json' )
-    .as( 'authData' )
-    .then( ( { credentials } ) => {
-      cy.signIn( credentials.admin.email, credentials.admin.password, options );
-    } );
+function signInAsAdmin(options?: SignInOptions): void {
+  cy.fixture('auth.json')
+    .as('authData')
+    .then(({ credentials }) => {
+      cy.signIn(credentials.admin.email, credentials.admin.password, options);
+    });
 }
 
-function signInAsUser( options?: SignInOptions ): void {
-  cy.fixture( 'auth.json' )
-    .as( 'authData' )
-    .then( ( { credentials } ) => {
-      cy.signIn( credentials.user.email, credentials.user.password, options );
-    } );
+function signInAsUser(options?: SignInOptions): void {
+  cy.fixture('auth.json')
+    .as('authData')
+    .then(({ credentials }) => {
+      cy.signIn(credentials.user.email, credentials.user.password, options);
+    });
 }
 
-function signUp( email: string, password: string ): void {
-  cy.visit( '/auth/sign-up' );
-  cy.get( 'input[type=email]' ).type( email );
-  cy.get( 'input[type=password]' ).first().type( password );
-  cy.get( 'input[type=password]' ).first().blur();
-  cy.get( 'input[type=password]' ).last().type( password );
-  cy.get( 'button[type=submit]' ).click();
+function signUp(email: string, password: string): void {
+  cy.visit('/auth/sign-up');
+  cy.get('input[type=email]').type(email);
+  cy.get('input[type=password]').first().type(password);
+  cy.get('input[type=password]').first().blur();
+  cy.get('input[type=password]').last().type(password);
+  cy.get('button[type=submit]').click();
 }
 
-function resetPassword( password: string, token: string ): void {
-  cy.visit( `/auth/reset-password?token=${token}` );
-  cy.get( 'input[type=password]' ).first().type( password );
-  cy.get( 'input[type=password]' ).first().blur();
-  cy.get( 'input[type=password]' ).last().type( password );
-  cy.get( 'button[type=submit]' ).click();
+function resetPassword(password: string, token: string): void {
+  cy.visit(`/auth/reset-password?token=${token}`);
+  cy.get('input[type=password]').first().type(password);
+  cy.get('input[type=password]').first().blur();
+  cy.get('input[type=password]').last().type(password);
+  cy.get('button[type=submit]').click();
 }
 
 function openUserMenu(): void {
-  cy.get( 'i.pi-user' ).click();
+  cy.get('i.pi-user').click();
 }
 
 function openProductsMenu(): void {
-  cy.get( 'i.pi-shopping-cart' ).click();
+  cy.get('i.pi-shopping-cart').click();
 }
 
 function logout(): void {
   cy.openUserMenu();
-  cy.get( '.p-button-label' ).contains( 'Logout' ).click();
+  cy.get('.p-button-label').contains('Logout').click();
 }
 
 function openAccountPage(): void {
   cy.openUserMenu();
-  cy.get( '.p-button-label' ).contains( 'Account' ).click();
+  cy.get('.p-button-label').contains('Account').click();
 }
 
 function openProfilePage(): void {
   cy.openUserMenu();
-  cy.get( 'li' ).contains( 'Profile' ).click();
+  cy.get('li').contains('Profile').click();
 }
 
 function openSettingsPage(): void {
   cy.openAccountPage();
-  cy.get( 'li' ).contains( 'Settings' ).click();
+  cy.get('li').contains('Settings').click();
 }
 
 function openProductCreatePage(): void {
   cy.openProductsMenu();
-  cy.get( 'li' ).contains( 'Create' ).click();
+  cy.get('li').contains('Create').click();
 }
 
-function registerNewUser( email: string, password: string ): void {
-  cy.signUp( email, password );
-  cy.contains( email );
-  cy.contains( 'Thank you for signing up' );
+function openProductEditPage(): void {
+  cy.openProductsMenu();
+  cy.get('li').contains('Edit').click();
+}
+
+function registerNewUser(email: string, password: string): void {
+  cy.signUp(email, password);
+  cy.contains(email);
+  cy.contains('Thank you for signing up');
 
   cy.task<any[]>(
     'connectDB',
     'SELECT * FROM public."user" ORDER BY id ASC',
-  ).then( ( users ) => {
-    const token = signToken( users[users.length - 1].id );
+  ).then((users) => {
+    const token = signToken(users[users.length - 1].id);
 
-    cy.visit( `/auth/confirm-email?token=${token}` );
-  } );
+    cy.visit(`/auth/confirm-email?token=${token}`);
+  });
 }
 
-function registerNewAdmin( email: string, password: string ): void {
-  cy.registerNewUser( email, password );
+function registerNewAdmin(email: string, password: string): void {
+  cy.registerNewUser(email, password);
   cy.task<any[]>(
     'connectDB',
     `UPDATE public."user" 
@@ -157,40 +168,76 @@ function registerNewAdmin( email: string, password: string ): void {
   );
 }
 
-function selectFromDropdown( dropdownId: string, value: string ): void {
-  cy.get( dropdownId ).click();
-  cy.get( '.p-dropdown-item' ).contains( value ).click();
+function selectFromDropdown(dropdownId: string, value: string): void {
+  cy.get(dropdownId).click('right');
+  cy.get('li[role="option"]').contains(value).click();
 }
 
-function fillProductForm( form: any ): void {
+function fillProductForm(form: any, uploadImage: boolean = true): void {
 
-  if ( form.categoryName && form.baseProductName ) {
-    cy.get( '[data-cy="new-product-category-toggle"]' ).click()
-    cy.get( '[data-cy="product-category-name"]' ).clear().type( form.categoryName );
-    cy.get( '[data-cy="product-base-product-name"]' ).clear().type( form.baseProductName );
-  } else if ( form.categorySelect && form.baseProductSelect ) {
-    cy.selectFromDropdown( '[data-cy="product-category-id"]', form.categorySelect );
-    cy.selectFromDropdown( '[data-cy="product-base-product-id"]', form.baseProductSelect );
-  } else {
-    cy.selectFromDropdown( '[data-cy="product-category-id"]', form.categorySelect );
-    cy.get( '[data-cy="new-base-product-toggle"]' ).click()
-    cy.get( '[data-cy="product-base-product-name"]' ).clear().type( form.baseProductName );
+  if (form.selectedProduct) {
+    cy.selectFromDropdown('[data-cy="product-list"]', form.selectedProduct);
   }
 
-  cy.get( '[data-cy="product-sku"]' ).clear().type( form.sku );
-  cy.get( '[data-cy="product-name-field"]' ).clear().type( form.name );
-  cy.get( '[data-cy="product-description"]' ).get( 'div[class="ql-editor ql-blank"]' ).focus().type( form.description );
+  if (form.categoryName && form.baseProductName) {
+    cy.get('[data-cy="new-product-category-toggle"]').click()
+    cy.get('[data-cy="product-category-name"]').clear().type(form.categoryName);
+    cy.get('[data-cy="product-base-product-name"]').clear().type(form.baseProductName);
+  } else if (form.categorySelect && form.baseProductSelect) {
+    cy.selectFromDropdown('[data-cy="product-category-id"]', form.categorySelect);
+    cy.selectFromDropdown('[data-cy="product-base-product-id"]', form.baseProductSelect);
+  } else if (form.categorySelect && form.baseProductName) {
+    cy.selectFromDropdown('[data-cy="product-category-id"]', form.categorySelect);
+    cy.get('[data-cy="new-base-product-toggle"]').click()
+    cy.get('[data-cy="product-base-product-name"]').clear().type(form.baseProductName);
+  }
 
-  cy.selectFromDropdown( '[data-cy="product-size"]', form.size );
-  cy.selectFromDropdown( '[data-cy="product-color"]', form.color );
+  cy.get('[data-cy="product-sku"]').clear().type(form.sku);
+  cy.get('[data-cy="product-name-field"]').clear().type(form.name);
+  cy.get('[data-cy="product-description"]').get('div[data-placeholder="Введіть опис для продукту"]').focus().clear().type(form.description);
 
-  cy.get( '[data-cy="product-prize"]' )
-    .type( Cypress._.repeat( '{leftArrow}', 5 ) )
-    .type( form.prize );
+  cy.selectFromDropdown('[data-cy="product-size"]', form.size);
+  cy.selectFromDropdown('[data-cy="product-color"]', form.color);
 
-  cy.get( '[data-cy="product-image-upload"]' )
-    .get( 'div[class="p-fileupload-content"]' )
-    .selectFile( 'cypress/files/product.jpeg', { action: 'drag-drop' } );
+  cy.get('[data-cy="product-prize"] input')
+    .type(Cypress._.repeat('{leftArrow}', 5))
+    .type(Cypress._.repeat('{del}', 1))
+    .type(form.prize);
+
+  if (uploadImage) {
+    uploadImageToProduct();
+  }
+}
+
+function uploadImageToProduct() {
+  cy.get('[data-cy="product-image-upload"]')
+    .get('div[class="p-fileupload-content"]')
+    .selectFile('cypress/files/product.jpeg', { action: 'drag-drop' });
+}
+
+function compareProductVariantFields(compareForm: any): void {
+  cy.get('[data-cy="product-sku"]').should('have.value', compareForm.sku);
+  cy.get('[data-cy="product-name-field"]').should('have.value', compareForm.name);
+  cy.get('[data-cy="product-description"]').get('div[data-placeholder="Введіть опис для продукту"]').should('have.text', compareForm.description);
+  cy.get('[data-cy="product-size"]').should('have.text', compareForm.size);
+  cy.get('[data-cy="product-color"]').should('have.text', compareForm.color);
+  cy.get('[data-cy="product-prize"] input').invoke('val')
+    .then((value) => {
+      expect(parseInt(value as string)).to.eq(compareForm.prize)
+    })
+}
+
+function compareProductCategoryAndBase(testProductCategory: string, testProductBase: string) {
+  cy.get('[data-cy="product-category-id"]').should('have.text', testProductCategory);
+  cy.get('[data-cy="product-base-product-id"]').should('have.text', testProductBase);
+}
+
+function checkImagesAmount(length: number) {
+  cy.get('[data-cy="product-images"]').find('img').should('have.length', length);
+}
+
+function markFirstImageToBeRemoved() {
+  cy.get('[data-cy="product-images"]').find('[data-cy="product-image-remove"]').first().click();
 }
 
 // ***********************************************
@@ -219,20 +266,26 @@ function fillProductForm( form: any ): void {
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add( 'signIn', signIn );
-Cypress.Commands.add( 'signInAsSuperAdmin', signInAsSuperAdmin );
-Cypress.Commands.add( 'signInAsAdmin', signInAsAdmin );
-Cypress.Commands.add( 'signInAsUser', signInAsUser );
-Cypress.Commands.add( 'signUp', signUp );
-Cypress.Commands.add( 'resetPassword', resetPassword );
-Cypress.Commands.add( 'registerNewAdmin', registerNewAdmin );
-Cypress.Commands.add( 'registerNewUser', registerNewUser );
-Cypress.Commands.add( 'logout', logout );
-Cypress.Commands.add( 'openUserMenu', openUserMenu );
-Cypress.Commands.add( 'openProductsMenu', openProductsMenu );
-Cypress.Commands.add( 'openAccountPage', openAccountPage );
-Cypress.Commands.add( 'openProfilePage', openProfilePage );
-Cypress.Commands.add( 'openSettingsPage', openSettingsPage );
-Cypress.Commands.add( 'openProductCreatePage', openProductCreatePage );
-Cypress.Commands.add( 'selectFromDropdown', selectFromDropdown );
-Cypress.Commands.add( 'fillProductForm', fillProductForm );
+Cypress.Commands.add('signIn', signIn);
+Cypress.Commands.add('signInAsSuperAdmin', signInAsSuperAdmin);
+Cypress.Commands.add('signInAsAdmin', signInAsAdmin);
+Cypress.Commands.add('signInAsUser', signInAsUser);
+Cypress.Commands.add('signUp', signUp);
+Cypress.Commands.add('resetPassword', resetPassword);
+Cypress.Commands.add('registerNewAdmin', registerNewAdmin);
+Cypress.Commands.add('registerNewUser', registerNewUser);
+Cypress.Commands.add('logout', logout);
+Cypress.Commands.add('openUserMenu', openUserMenu);
+Cypress.Commands.add('openProductsMenu', openProductsMenu);
+Cypress.Commands.add('openAccountPage', openAccountPage);
+Cypress.Commands.add('openProfilePage', openProfilePage);
+Cypress.Commands.add('openSettingsPage', openSettingsPage);
+Cypress.Commands.add('openProductCreatePage', openProductCreatePage);
+Cypress.Commands.add('openProductEditPage', openProductEditPage);
+Cypress.Commands.add('selectFromDropdown', selectFromDropdown);
+Cypress.Commands.add('fillProductForm', fillProductForm);
+Cypress.Commands.add('compareProductVariantFields', compareProductVariantFields);
+Cypress.Commands.add('compareProductCategoryAndBase', compareProductCategoryAndBase);
+Cypress.Commands.add('uploadImageToProduct', uploadImageToProduct);
+Cypress.Commands.add('checkImagesAmount', checkImagesAmount);
+Cypress.Commands.add('markFirstImageToBeRemoved', markFirstImageToBeRemoved);
