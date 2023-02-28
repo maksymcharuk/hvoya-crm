bdf3cd0a0a3e1a57874181b927ee405052ce621c
Maksym Charuk <maxcharuk@gmail.com>
2023-02-27 21:31:26 -0500

    Fix seeds for User

--- src/modules/database/seeds/users.seed.ts
+++ src/modules/database/seeds/users.seed.ts
@@ -7,15 +7,17 @@ import { Role } from '../../../enums/role.enum';
 export default class UserSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
     const repository = dataSource.getRepository(UserEntity);
-    const password = '12345';
-    await repository.insert([
+    const usersData: Partial<UserEntity>[] = [
       {
         firstName: 'John',
         lastName: 'SuperAdmin',
+        middleName: 'Smith',
         email: 'john-super-admin@email.com',
-        password: password,
+        password: 'Admin12345',
         role: Role.SuperAdmin,
         emailConfirmed: true,
+        userConfirmed: true,
+        userFreezed: false,
         phoneNumber: '0673344123',
         location: 'Lviv',
         bio: 'I am a super admin',
@@ -25,10 +27,13 @@ export default class UserSeeder implements Seeder {
       {
         firstName: 'Alice',
         lastName: 'Admin',
+        middleName: 'Anderson',
         email: 'alice-admin@email.com',
-        password: password,
+        password: 'Admin12345',
         role: Role.Admin,
         emailConfirmed: true,
+        userConfirmed: true,
+        userFreezed: false,
         phoneNumber: '0671234567',
         location: 'New York',
         bio: 'I am an admin',
@@ -38,16 +43,20 @@ export default class UserSeeder implements Seeder {
       {
         firstName: 'Peter',
         lastName: 'User',
+        middleName: 'Parker',
         email: 'peter-user@email.com',
-        password: password,
+        password: 'User12345',
         role: Role.User,
         emailConfirmed: true,
+        userConfirmed: true,
+        userFreezed: false,
         phoneNumber: '0679876543',
         location: 'Lutsk',
         bio: 'I am a user',
         cardNumber: '5218 5722 2223 2634',
         cardholderName: 'Peter User',
       },
-    ]);
+    ];
+    await repository.insert(usersData);
   }
 }
