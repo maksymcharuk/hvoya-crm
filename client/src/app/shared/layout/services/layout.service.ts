b18659870bd90a243e085bf774fb9d7a71303a05
Maksym Charuk <maxcharuk@gmail.com>
2023-03-03 16:23:11 -0500

    Add brand theme 'Hvoya'

--- client/src/app/shared/layout/services/layout.service.ts
+++ client/src/app/shared/layout/services/layout.service.ts
@@ -29,7 +29,7 @@ export class LayoutService {
     inputStyle: 'outlined',
     menuMode: 'static',
     colorScheme: 'light',
-    theme: 'lara-light-indigo',
+    theme: 'hvoya',
     scale: 14,
   };
 
