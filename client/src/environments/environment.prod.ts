1689d129a4506fbf9d6f372641e9ad9ff2c30c4d
Maksym Charuk <maxcharuk@gmail.com>
2023-05-26 12:11:59 -0400

    Hotfix: add privatBankPaymentUrl for all envs

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   webSocketUrl: '',
   apiUrl: '/api',
+  privatBankPaymentUrl:
+    'https://next.privat24.ua/payments/form/%7B%22token%22%20:%20%22410bacd5bfd6274a1d45676afaa6ddc2aue0g3oc%22%7D',
 };
--- client/src/environments/environment.test.ts
+++ client/src/environments/environment.test.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false,
   webSocketUrl: 'https://localhost:3001',
   apiUrl: 'https://localhost:3001/api',
+  privatBankPaymentUrl:
+    'https://next.privat24.ua/payments/form/%7B%22token%22%20:%20%22410bacd5bfd6274a1d45676afaa6ddc2aue0g3oc%22%7D',
 };
 
 /*
