423b8ecfc3401b459de1afc5851cf3c54ad086ff
Maksym Charuk <maxcharuk@gmail.com>
2023-07-19 17:23:01 -0400

    Tun on cron job for DB backups

--- src/modules/database/services/database-tasks.service.ts
+++ src/modules/database/services/database-tasks.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
-// import { Cron, CronExpression } from '@nestjs/schedule';
-
-// import { DatabaseService } from './database.service';
+import { DatabaseService } from './database.service';
 
 @Injectable()
 export class DatabaseTasksService {
-  //   @Cron(CronExpression.EVERY_DAY_AT_5AM)
-  //   async backupDatabase() {
-  //     return DatabaseService.backup();
-  //   }
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async backupDatabase() {
+    return DatabaseService.backup();
+  }
 }
