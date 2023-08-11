1b08f2c602331b0c0df5e4b14d02976214969e4a
Maksym Charuk <maxcharuk@gmail.com>
2023-08-11 12:36:02 -0400

    Clean up test code

--- src/modules/integrations/delivery/delivery.module.ts
+++ src/modules/integrations/delivery/delivery.module.ts
@@ -1,7 +1,6 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
-import { DeliveryController } from './delivery.controller';
 import { DeliveryServiceFactory } from './factories/delivery-service/delivery-service.factory';
 import { DeliveryTasksService } from './services/delivery-tasks/delivery-tasks.service';
 import { NovaPoshtaApiService } from './services/nova-poshta-api/nova-poshta-api.service';
@@ -9,7 +8,6 @@ import { UkrPoshtaApiService } from './services/ukr-poshta-api/ukr-poshta-api.se
 
 @Module({
   imports: [HttpModule],
-  controllers: [DeliveryController],
   providers: [
     NovaPoshtaApiService,
     UkrPoshtaApiService,
