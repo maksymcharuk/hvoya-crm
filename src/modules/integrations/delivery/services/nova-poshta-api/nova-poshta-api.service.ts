d39c8c4f9b49a12f48758c7fa3a41330bac7a08d
Maksym Charuk <maxcharuk@gmail.com>
2023-09-21 16:56:11 -0400

    Fix issue with updating delivery status for Nova Poshta when address was changed

--- src/modules/integrations/delivery/services/nova-poshta-api/nova-poshta-api.service.ts
+++ src/modules/integrations/delivery/services/nova-poshta-api/nova-poshta-api.service.ts
@@ -9,6 +9,7 @@ import {
   GetDeliveryStatusesDto,
   GetDeliveryStatusesResponse,
 } from '@interfaces/delivery';
+import { DeliveryServiceRawStatus } from '@interfaces/delivery/get-delivery-statuses.response';
 import {
   NovaPoshtaGetDeliveryStatusesRequest,
   NovaPoshtaGetDeliveryStatusesResponse,
@@ -31,7 +32,44 @@ export class NovaPoshtaApiService extends DeliveryApiService {
     super();
   }
 
-  getDeliveryStatuses(
+  async getDeliveryStatuses(
+    getDeliveryStatusesDto: GetDeliveryStatusesDto,
+  ): Promise<GetDeliveryStatusesResponse> {
+    const res = await this.getDeliveryStatusesInternal(getDeliveryStatusesDto);
+    const statusesNewTrackingId = res.statuses.filter(
+      (status) => status.newTrackingId,
+    );
+
+    // If delivery address was changed when a parcel was in transit
+    // then we need to get statuses for the last created on the basis number
+    // as Nova Poshta created new tracking number for the new address
+    if (statusesNewTrackingId.length !== 0) {
+      const statuses = await this.getDeliveryStatusesInternal({
+        trackingInfo: statusesNewTrackingId.map((status) => ({
+          trackingId: status.newTrackingId as string,
+          phone: '',
+        })),
+      });
+
+      return new GetDeliveryStatusesResponse({
+        statuses: res.statuses.map((status) => {
+          const newTrackingIdStatus = statuses.statuses.find(
+            (s) => s.trackingId === status.newTrackingId,
+          );
+
+          return new DeliveryServiceRawStatus({
+            ...status,
+            status: newTrackingIdStatus?.status ?? status.status,
+            rawStatus: newTrackingIdStatus?.rawStatus ?? status.rawStatus,
+          });
+        }),
+      });
+    }
+
+    return res;
+  }
+
+  private getDeliveryStatusesInternal(
     getDeliveryStatusesDto: GetDeliveryStatusesDto,
   ): Promise<GetDeliveryStatusesResponse> {
     return this.makeApiCall(
@@ -63,6 +101,7 @@ export class NovaPoshtaApiService extends DeliveryApiService {
                   date: new Date(status.TrackingUpdateDate).toString(),
                   status: getStatus(status.Status),
                   rawStatus: status.Status,
+                  newTrackingId: status.LastCreatedOnTheBasisNumber,
                 })),
               });
             },
