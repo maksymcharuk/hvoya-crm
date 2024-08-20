21c29dd983aee8ba444c9d43433b75c94748787d
Vladyslav Charuk <vlad.charu@gmail.com>
2024-08-20 12:48:34 +0300

    prod env change

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   webSocketUrl: '',
   apiUrl: '/api',
   // Old privat link: 'https://next.privat24.ua/payments/form/%7B%22token%22%20:%20%22410bacd5bfd6274a1d45676afaa6ddc2aue0g3oc%22%7D'
+  // Broken link 'https://next.privat24.ua/payments/form/%7B%22companyID%22:%224563786%22%7D'
   privatBankPaymentUrl:
-    'https://next.privat24.ua/payments/form/%7B%22companyID%22:%224563786%22%7D',
+    'https://next.privat24.ua/payments/form/%7B%22token%22%20:%20%22410bacd5bfd6274a1d45676afaa6ddc2aue0g3oc%22%7D',
 };
